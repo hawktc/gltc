f79326c4c4d262247af26a2e5994a8619dae74b7
TAN CHAO - HAWK <TAN.CHAO.HAWK@GMAIL.COM>
2012-07-15 23:04:30 +0800

    start py study

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ gltc
 ====
 
 learning for career skills, mainly about python and unix
+
+2012.07.15
+---
+http://woodpecker.org.cn/abyteofpython_cn/chinese/
+
+TODO list:
+---
+Init a website with python and django
+Init a website with ruby on rails
+Show my programming learning during my extra time
---
+++ py/note.py
@@ -0,0 +1,19 @@
+# notes for <A little byte of Python>
+# hawk c tan
+# 2012-07-15
+#!/usr/bin/env python
+
+# get user input
+# print input("Please input a number: ")
+# print raw_input("Please input another unmber: ")
+
+# for loop with range() # range(1, 5) ~ 1 <= i < 5
+for i in range(1, 5):
+    print i
+else:
+    print "end of for loop"
+
+for i in range(1, 7, 2):
+    print i
+else:
+    print "end of for 2 loop"
